6c6a0c684acdc7ed9262be23838a51f4c0e66ab5
Thong Tran <tranthon@pdx.edu>
2021-07-01 13:55:18 -0500

    update

---
+++ MyStudentMgmtApp/src/edu/mum/cs/cs425/studentmgmt/StudentMgmtApp.java
@@ -0,0 +1,11 @@
+package edu.mum.cs.cs425.studentmgmt;
+
+public class StudentMgmtApp {
+    public static void main(String[] args) {
+
+    }
+
+    public static void saveStudent() {
+        
+    }
+}
